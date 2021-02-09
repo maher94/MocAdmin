4f4f6ce329caa1223b014fdd711d15deea9632a0
mamoussa <mamoussa@vermeg.com>
2021-02-10 00:02:06 +0100

    add build project

---
+++ build/precache-manifest.1e4b1282377adb8778589ebe2a75c48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1115ac3a188bb8f599fd9e8ba57b3397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57bfb794505c763342a4",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "57bfb794505c763342a4",
+    "url": "/static/js/2.eccd187c.chunk.js"
+  },
+  {
+    "revision": "ae627bba94faa84b1ad672a48bac49fe",
+    "url": "/static/js/2.eccd187c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ddfd71a954ac41bb7b",
+    "url": "/static/js/main.2fd8ecc6.chunk.js"
+  },
+  {
+    "revision": "02abd13eaf1d7c33000d",
+    "url": "/static/js/runtime-main.6a771f16.js"
+  }
+]);
